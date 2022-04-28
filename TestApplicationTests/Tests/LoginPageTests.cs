6625b6e73d2056de5d1065ba4e7054d409e05a37
gshimabuko <gshimabuko@gmail.com>
2022-04-27 21:23:53 -0300

    Adding Assertion to first test case on login screen

--- TestApplicationTests/Tests/LoginPageTests.cs
+++ TestApplicationTests/Tests/LoginPageTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using CommonLibs.Implementation;
 using TestApplicationTests.Tests;
+using System.Threading;
 
 namespace TestApplicationTests.tests
 {
@@ -10,6 +11,21 @@ namespace TestApplicationTests.tests
         public void VerifyLoginTest()
         {
             loginPage.LoginToApplication("test@example.com","Test@12345");
+            Thread.Sleep(3000);
+            string expectedTitle = "Home Page - My ASP.NET Application";
+            Thread.Sleep(3000);
+            string actualTitle = CmnDriver.GetPageTitle();
+            Assert.AreEqual(expectedTitle,actualTitle);
+        }
+        [Test]
+        public void VerifyLoginTest2()
+        {
+            loginPage.LoginToApplication("test@example.com","Test@1234");
+            Thread.Sleep(3000);
+            string expectedTitle = "Home Page - My ASP.NET Application";
+            Thread.Sleep(3000);
+            string actualTitle = CmnDriver.GetPageTitle();
+            Assert.AreEqual(expectedTitle,actualTitle);
         }
     }
 }
